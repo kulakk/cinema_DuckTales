631c4e6a36082289c92f2e1c867b04bf06e195b7
kulakk <k.kulak23@gmail.com>
2015-11-30 01:00:25 +0100

    -zakomentowane zbędne tabele, panele i panel logowania visitora -logowanie dla admina w panelu nr 3 na samym dole -dołożone przyciski rezerwacji, ale bez podlinkowania do formularza -obrazy w galerii zmienione na divy -zupełnie nowy panel admina

---
+++ choose.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+
+<form method="post" enctype="multipart/form-data">
+  <!-- informuje ze bede opisywal formularz przekazywany metoda POST i kodowany w sposob umożliwiajacy przesyłanie plików -->
+<input name="nazwa" placeholder="nazwa przepisu"/><br/>
+<!-- opisuje kontrolke wejscia danych ktore beda przekazane przez POST do pola tablicowego o takiej samej nazwie. dodaje wskazowke dla uzytkownika czeg o dotyczy kontrolka   -->
+<textarea name="opis" placeholder="opis wykonania" ></textarea><br/>
+<!-- textarea to specjalna kontrolka wejscia dla dluzszego dekstu wieloliniowego -->
+<input type="file" name="zdjecie"><br/>
+<!-- informuje o typie plikowym -->
+<select name="trudnosc">
+<option value="latwe" label="Łatwe">Łatwe</option>
+<option value="srednio trudne" label="Średnio trudne">Średnio trudne</option>
+<option value="trudne" label="Trudne">Trudne</option>
+</select>
+<!-- opisuje kontrolke wyboru z mozlliwymi opcjami, wartosc  z value opcji bedzie wartoscia pola tablicowego o nazwie kon
+
+
+
+ </body>
+ </html>
